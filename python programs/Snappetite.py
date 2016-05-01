331abe8f282558240a79e455d3fc629a7efdc151
hcwoo2 <hcwoo2@illinois.edu>
2016-05-01 15:17:15 -0500

    Update Snappetite.py
    Added the option to see the graph and the data point of your specific food.

--- python programs/Snappetite.py
+++ python programs/Snappetite.py
@@ -48,3 +48,66 @@ for i in xrange(0,3):
 
 print average
 print(str(clf.predict([average])))
+
+####################################
+########## 3D Graphing #############
+####################################
+
+showGraph = raw_input('Show graph? [y/n]\n')
+status = True
+while(status):
+	showGraph.lower()#case insensitive
+	if showGraph == 'y':
+		showGraph = True
+		status = False
+	elif showGraph == 'n':
+		showGraph = False
+		status = False
+	else:
+		showGraph = raw_input('Invalid input, show graph? [y/n]\n')
+
+while(showGraph):
+	import matplotlib.pyplot as plt
+	from mpl_toolkits.mplot3d import Axes3D
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection = '3d')
+
+	with open('RGBValue.csv','rb') as f:
+		reader = csv.reader(f)
+		RGBValue = list(reader)
+
+	with open('foodNames.csv','rb') as f:
+		reader = csv.reader(f)
+		foodNameTemp = list(reader)
+
+	foodName = []
+	for i in foodNameTemp:
+		foodName += i
+
+	pairList = zip(foodName,RGBValue)#pair foods with their values
+
+	colorMap = {}#assign each food its own color
+
+	for i in pairList:
+		if (i[0] not in colorMap):
+			colorMap[i[0]] = [[np.random.rand()],[np.random.rand()],[np.random.rand()]]#set a random color to each food
+
+
+	for i in pairList:
+		x = float(i[1][0])
+		y = float(i[1][1])
+		z = float(i[1][2])
+		ax.scatter(x, y, z, c = colorMap[i[0]], label = i[0])#plot on 3d graph
+	
+	ax.scatter(average[0], average[1], average[2], c = 'r', marker = 'x', label = 'Your Food')#plot the food that was given
+
+	ax.set_xlabel('Red')#set axis labels
+	ax.set_ylabel('Green')
+	ax.set_zlabel('Blue')
+
+	#position legend
+	plt.legend(loc='upper left', numpoints=1, ncol=3, fontsize=8, bbox_to_anchor=(0, 0))
+
+	plt.show()#display graph
+
+	showGraph = False
