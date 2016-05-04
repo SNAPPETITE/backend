40b6b6f612f2a7105eb4f6acbecb10684ed12a2c
Andrew P. Caldwell <espio301@gmail.com>
2016-05-04 02:28:50 -0500

    Update scikitlearn.py
    Adding functionalities (color averaging) to the whole machine learning algorithm

--- app/scikitlearn.py
+++ app/scikitlearn.py
@@ -1,4 +1,5 @@
 from sklearn import linear_model
+from PIL import Image
 #probably wanna make a whole program to scrape the image for the different sections
 #which have food on the plate and then save each section as its own class with rgb values.
 #But I just figured scrambled eggs are yellow (255,255,0) so heres a small program 
@@ -7,12 +8,29 @@ from sklearn import linear_model
 #real dataset would be filled with the a class but for example purposes I
 #just used a pixel to represent each food.
 
+im = Image.open("photos/mix.jpg")
+pic = im.load()
+x=im.width
+y=im.height
+
+
+colors = [0,0,0]
+for i in range(0,y):
+	for j in range(0,x):
+		colors[0] += pic[i,j][0]
+		colors[1] += pic[i,j][1]
+		colors[2] += pic[i,j][2]
+
+red = colors[0]/(x * y)
+green = colors[1]/(x *y)
+blue = colors[2]/(x * y)
+
 #dataset is filled with 2 yellows and a black. yellow -> 1 black -> 2
 dataset = [[255,255,0],[255,255,0],[0,0,0]]
 target = [1,1,2]
 
 #test would be the user's picture. in this case, its a scrambled egg
-test = [[255,255,0]]
+test = [[red,green,blue]]
 
 #set up the log reg model under the var logreg. then fit the model with dataset
 #and target
@@ -21,4 +39,4 @@ logreg.fit(dataset, target)
 
 #prints the prediction using the logistic regression model.
 result = logreg.predict(test)
-print(result)
+
